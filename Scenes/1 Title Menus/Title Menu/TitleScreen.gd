391e85646928549bb4fbe275102e9d4d9d8a1b67
Bryon Schwausch <160765995+Teslakore1@users.noreply.github.com>
2024-03-07 12:22:27 -0600

    Button Function
    Title screen buttons added functionality
    Options menu opens and closes

---
+++ Scenes/1 Title Menus/Title Menu/TitleScreen.gd
@@ -0,0 +1,28 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+# BS Functions added for button funtion in order the buttons show up on the title menu.
+func _on_save_select_button_pressed():
+	$GameSaves.show()
+
+func _on_options_button_pressed():
+	$OptionsMenu.show()
+
+func _on_close_button_pressed():
+	get_tree().quit()
+# BS Button functions end
+
+
+
+
+
+
