83438c3d0e728388255e7f9994199720334bfbe2
Teslakore1 <bryonschwausch1@gmail.com>
2024-03-13 11:41:03 -0500

    Function full screen

--- Scenes/1 Title Menus/Options Menu/Scripts/WM-OptionButton.gd
+++ Scenes/1 Title Menus/Options Menu/Scripts/WM-OptionButton.gd
@@ -11,13 +11,16 @@ func _process(delta):
 	pass
 
 func add_items():
-	$".".add_item("Fullscreen")
 	$".".add_item("Windowed")
+	$".".add_item("Fullscreen")
+	
 
 func _on_item_selected(index):
 	var current_selected = index
 	
 	if current_selected == 0:
-		DisplayServer.window_set_size(Vector2(2560,1440))
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
 	if current_selected == 1:
-		DisplayServer.window_set_size(Vector2(2560,1440))
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	
+
